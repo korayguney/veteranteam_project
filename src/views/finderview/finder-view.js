91b0c940a5be3fbd011da7263e14a31ef1372163
zekicelik <celik_zeki@yahoo.com>
2017-03-30 23:56:59 +0300

    Add finderview without map

---
+++ src/views/finderview/finder-view.js
@@ -0,0 +1,42 @@
+import { View, __, Sidebar, ViewManager, NavBar } from 'erste';
+import MainView from '../main-view';
+import LoggedInView from "../loggedin/loggedin-view";
+import MapView from "../map/map-view";
+
+
+class FinderView extends View {
+    constructor() {
+        super();
+        this.navBar = new NavBar({
+            title: __('Welcome to beveteran'),
+            hasMenuButton: true,
+            hasBackButton: true
+        });
+        this.hasSidebar = true;
+
+    }
+
+    onActivation() {
+        if (cfg.PLATFORM == 'device')
+            StatusBar.styleDefault();
+    }
+
+
+
+
+    template() {
+        return `
+<view>
+    ${this.navBar}
+    <div class="map">
+        <div class="imgcontainer">
+            <img src="static/img/logo.png" alt="Avatar" class="logo">
+        </div>
+<!-- map view will be here -->
+    </div>
+</view>
+`;
+    }
+}
+
+module.exports = FinderView;
